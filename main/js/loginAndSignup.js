0fb781f5f2fc324777a17fd09ff691789362febf
faria-karim-porna <faria.porna.08@gmail.com>
2022-02-09 20:39:56 +0000

    Login And Signup HTML, CSS, JS Added, Resources Added, Common CSS Added

---
+++ main/js/loginAndSignup.js
@@ -0,0 +1,9 @@
+function loginSwitch() {
+  document.getElementById("sign-up-area").style.display = "none";
+  document.getElementById("login-area").style.display = "block";
+}
+
+function signupSwitch() {
+  document.getElementById("login-area").style.display = "none";
+  document.getElementById("sign-up-area").style.display = "block";
+}
---
+++ resources.md
@@ -0,0 +1,17 @@
+color pallet
+
+.first-color { 
+	background: #00204a; 
+}
+	
+.second-color { 
+	background: #005792; 
+}
+
+.third-color { 
+	background: #00bbf0; 
+}
+
+.fourth-color { 
+	background: #fdb44b; 
+}
